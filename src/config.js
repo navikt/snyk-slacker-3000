b6864498e1b7048bd42b57fd14b672f514134a38
J-K. Solbakken <jksolbakken@gmail.com>
2022-11-10 13:21:27 +0100

    make config location configurable

--- src/config.js
+++ src/config.js
@@ -1,14 +1,27 @@
+const fs = require("fs");
+const { env } = require("process");
+
 const log = require("./log");
 
 const mappingByOrg = {};
 
-require("../config.json").forEach((config) => {
+const defaultPathToConf = "./config.json";
+const pathToConf = env.CONF_LOCATION || defaultPathToConf;
+if (!fs.existsSync(pathToConf)) {
+  throw `cannot find config at at the default location '${defaultPathToConf}' or at $CONF_LOCATION ('${process.env.CONF_LOCATION}')`;
+}
+
+JSON.parse(fromLocalFile(pathToConf)).forEach((config) => {
   const { snykOrg } = config;
   if (mappingByOrg[snykOrg]) {
     log.warn(`Multiple configs found for org ${snykOrg}`);
   }
   mappingByOrg[snykOrg] = config;
 });
+
+function fromLocalFile(path) {
+  return fs.readFileSync(path);
+}
 
 function getConfigForOrg(snykOrg) {
   return mappingByOrg[snykOrg];
